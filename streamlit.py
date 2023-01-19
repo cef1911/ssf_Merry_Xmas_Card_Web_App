34cf25a2a3048f692acb876a857bdb0e76ca4dd3
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-19 06:29:53 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,20 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import altair as alt
 
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
+
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+c = alt.Chart(chart_data).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.altair_chart(c, use_container_width=True)
