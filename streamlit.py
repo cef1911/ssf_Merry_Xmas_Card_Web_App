5f2992797bfee18d3cc68de8c58f96b53522bfef
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-19 06:35:40 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -26,3 +26,12 @@ chart_data2 = pd.DataFrame(
     columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data2)
+
+txt = st.text_area('Demo of Chris F Text to analyze', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+st.write('Sentiment:', run_sentiment_analysis(txt))
