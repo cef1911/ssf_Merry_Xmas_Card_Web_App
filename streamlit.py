cf8f5aa842beddf73a1c4e37a21ba1691a0755e5
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-19 06:21:40 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.write('hello world!')
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
