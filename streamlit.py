84b91352b5e5cea5c14218eb38805f7b7009b78b
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-19 06:33:31 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -19,3 +19,10 @@ c = alt.Chart(chart_data).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
 st.altair_chart(c, use_container_width=True)
+
+
+chart_data2 = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data2)
