c31d6ffd698f33caee9e2da203f6aae1089ed9b9
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-19 06:40:16 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,6 +4,9 @@ import numpy as np
 import altair as alt
 import pandas as pd
 
+
+st.write("Streamlit Play again:) creating dataframes, histogram, scatterplot, and matplotlib visualizationa")
+
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
