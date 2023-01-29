f6c84bc6c6102c77d41987fd4d1941d7a7fa549d
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-29 17:33:48 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,37 +12,20 @@ image = Image.open("sunrise.jpeg")
 
 st.image(image, caption='Amanda Franklin, Happy Heavenly Birthday')
 
-
-st.write("Streamlit Play again:) creating dataframes, histogram, scatterplot, and matplotlib visualizations")
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
+st.write("Tribute To Amanda Franklin")
 
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+imagetwo = Image.open("birthday.jpeg")
 
-c = alt.Chart(chart_data).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-
-st.altair_chart(c, use_container_width=True)
+st.image(imagetwo, caption='Amanda Franklin')
 
 
-chart_data2 = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+imagethree = Image.open("memories.png")
 
-st.line_chart(chart_data2)
+st.image(imagethree, caption='Amanda Franklin')
 
-txt = st.text_area('Demo of Chris F Text to analyze', '''
-    It was the best of times, it was the worst of times, it was
-    the age of wisdom, it was the age of foolishness, it was
-    the epoch of belief, it was the epoch of incredulity, it
-    was the season of Light, it was the season of Darkness, it
-    was the spring of hope, it was the winter of despair, (...)
-    ''')
-#st.write('Sentiment:', run_sentiment_analysis(txt))
+
+st.write("Love the Franklins, Chris")
+
+
+
